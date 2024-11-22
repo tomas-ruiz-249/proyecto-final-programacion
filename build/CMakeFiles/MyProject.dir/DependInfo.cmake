2af40fcd9e9b0ebb6de6071eb471b5e9f8090f41
tomas <tomas.ruizcorrea@gmail.com>
2024-11-22 02:46:29 -0500

    added cmake, organized files

---
+++ build/CMakeFiles/MyProject.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/AmmoBox.cpp" "CMakeFiles/MyProject.dir/src/AmmoBox.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/AmmoBox.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Animation.cpp" "CMakeFiles/MyProject.dir/src/Animation.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Animation.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Canvas.cpp" "CMakeFiles/MyProject.dir/src/Canvas.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Canvas.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Drawable.cpp" "CMakeFiles/MyProject.dir/src/Drawable.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Drawable.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Enemy.cpp" "CMakeFiles/MyProject.dir/src/Enemy.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Enemy.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/EnemyManager.cpp" "CMakeFiles/MyProject.dir/src/EnemyManager.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/EnemyManager.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Entity.cpp" "CMakeFiles/MyProject.dir/src/Entity.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Entity.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/FileReader.cpp" "CMakeFiles/MyProject.dir/src/FileReader.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/FileReader.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Game.cpp" "CMakeFiles/MyProject.dir/src/Game.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Game.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/HealthBox.cpp" "CMakeFiles/MyProject.dir/src/HealthBox.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/HealthBox.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Item.cpp" "CMakeFiles/MyProject.dir/src/Item.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Item.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/ItemManager.cpp" "CMakeFiles/MyProject.dir/src/ItemManager.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/ItemManager.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Lamp.cpp" "CMakeFiles/MyProject.dir/src/Lamp.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Lamp.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Map.cpp" "CMakeFiles/MyProject.dir/src/Map.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Map.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/MeleeEnemy.cpp" "CMakeFiles/MyProject.dir/src/MeleeEnemy.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/MeleeEnemy.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Player.cpp" "CMakeFiles/MyProject.dir/src/Player.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Player.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Projectile.cpp" "CMakeFiles/MyProject.dir/src/Projectile.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Projectile.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/RangedEnemy.cpp" "CMakeFiles/MyProject.dir/src/RangedEnemy.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/RangedEnemy.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Raycaster.cpp" "CMakeFiles/MyProject.dir/src/Raycaster.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Raycaster.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/SoundManager.cpp" "CMakeFiles/MyProject.dir/src/SoundManager.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/SoundManager.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/TextureManager.cpp" "CMakeFiles/MyProject.dir/src/TextureManager.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/TextureManager.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/Weapon.cpp" "CMakeFiles/MyProject.dir/src/Weapon.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/Weapon.cpp.o.d"
+  "/home/tomas/Documents/uni/code/proyecto-final-programacion/src/main.cpp" "CMakeFiles/MyProject.dir/src/main.cpp.o" "gcc" "CMakeFiles/MyProject.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Projectile.cpp
@@ -0,0 +1,66 @@
+#include "Projectile.h"
+#include "TextureManager.h"
+#include "RayCaster.h"
+#include <iostream>
+#include <cmath>
+#include "SoundManager.h"
+
+Projectile::Projectile(Point2D pos, double ang, int damage)
+{
+	position = pos;
+	speed = 5;
+	angle = ang;
+	timer = 0;
+	timeLimit = 10;
+	this->damage = damage;
+
+	sprite = new Animated();
+	sprite->scale = 0.5;
+	sprite->shift = 0;
+
+	TextureManager* texMgr = TextureManager::getInstance();
+	Animation fireball = {};
+	fireball.texture = texMgr->getTexture("sprites\\animated\\fireball.png");
+	fireball.numFrames = 2;
+	fireball.animationSpeed = 2;
+	sprite->animations.push_back(fireball);
+}
+
+bool Projectile::shoot()
+{
+	RayCaster raycaster;
+	Player* player = Player::getInstance();
+	Map map = *Map::getInstance();
+
+	RayCastResult rayToWall = raycaster.rayCast(angle, position, map);
+	double distanceToPlayer = sprite->getDistanceFromPlayer(position, *player);
+
+	bool projectileCrash = distanceToPlayer < 0.5 or rayToWall.depth < 0.5 or timer > timeLimit;
+	if (projectileCrash) {
+		SoundManager* soundManager = SoundManager::getInstance();
+		Sound explosion = soundManager->getSound("projectile_explode.mp3");
+		Sound projectileFlying = soundManager->getSound("zombie_shoot");
+		if (IsSoundPlaying(projectileFlying)) {
+			StopSound(projectileFlying);
+		}
+		PlaySound(explosion);
+		if (distanceToPlayer < 0.5) {
+			player->takeDamage(damage);
+		}
+		return true;
+	}
+	else {
+		Point2D d;
+		d.x = cos(angle) * speed * GetFrameTime();
+		d.y = sin(angle) * speed * GetFrameTime();
+		position.x += d.x;
+		position.y += d.y;
+		timer += GetFrameTime();
+		return false;
+	}
+}
+
+Point2D Projectile::getPosition()
+{
+	return position;
+}
